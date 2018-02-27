55826485de94599f576df933271649262d821699
[GucciGerm] <[germaine.balmania@gmail.com]>
2018-02-27 02:22:17 +0000

    Adding our header file

---
+++ holberton.h
@@ -0,0 +1,27 @@
+#ifndef Rezz
+#define Rezz
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * struct operations - Here
+ * @j: pointer to character
+ * @func: pointer to a function
+ */
+
+typedef struct operations
+{
+	char *j;
+	int (*func)(va_list printafun);
+} ope;
+
+int _printf(const char *format, ...);
+int _printchar(va_list gas);
+int _printstring(va_list gas);
+int _printmodo(va_list gas);
+int _strlen(char *s);
+int _putchar(char c);
+#endif
